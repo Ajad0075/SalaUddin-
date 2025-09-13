cee3fe4fec662b715b6de9713d47ae2ba2e6c0d9
Ajad0075 <kinghk549@gmail.com>
2025-09-13 05:50:55 +0530

    Update Helpall.js

--- Script/commands/Helpall.js
+++ Script/commands/Helpall.js
@@ -36,7 +36,7 @@ ${allCommands.map(cmd => `║ ➔ ${cmd}`).join("\n")}
 
  
  const backgrounds = [
- "https://i.imgur.com/u3XbMyG.jpeg
+ "https://i.imgur.com/u3XbMyG.jpeg"
  ];
  const selectedBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
  const imgPath = __dirname + "/cache/helpallbg.jpg";
